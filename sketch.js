8ecbcbae524a06a26adb5c817d2d918c915ee65f
Lau1306 <70720577+Lau1306@users.noreply.github.com>
2021-10-13 00:12:55 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+//Crea namespacing para usar librería Matter
+
+//Define todas las variables necesarias
+
+function setup() {
+  createCanvas(3000,800);
+
+  //Crea motor físico y mundo
+
+  //Crea un objeto ground a partir de una clase
+
+  //Crea 20 objetos box a partir de una clase
+
+  //Crea un objeto ball a partir de una clase
+
+  //Crea un objeto rope a partir de una clase y lo une a ball
+}
+
+function draw() {
+  background(0); 
+
+  //Actualiza motor físico
+  
+  //Dibuja el objeto ground
+
+  //Dibuja los objetos box
+
+  //Dibuja el objeto ball
+
+  //Dibuja el objeto rope
+}
+
+//Evento para arrastar la bola de demolición con el mouse
